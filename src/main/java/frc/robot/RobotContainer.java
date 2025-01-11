7d0dbb9af69683f51f3d4ad12650c54963c5f3ce
MADMAN-Modding <mad@kmdtech.net>
2025-01-11 10:31:16 -0500

    reverted names

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,9 +8,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.command.Drive;
-import frc.robot.subsystem.DriveSub;
-import frc.robot.subsystem.VisionSub;
+import frc.robot.commands.Drive;
+import frc.robot.subsystems.DriveSub;
+import frc.robot.subsystems.VisionSub;
 
 public class RobotContainer {
   private final Joystick xboxController = new Joystick(0);
--- src/main/java/frc/robot/commands/MaintainAll.java
+++ src/main/java/frc/robot/commands/MaintainAll.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.command;
+package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystem.DriveSub;
-import frc.robot.subsystem.VisionSub;
+import frc.robot.subsystems.DriveSub;
+import frc.robot.subsystems.VisionSub;
 
 public class MaintainAll extends Command {
   VisionSub visionSub;
