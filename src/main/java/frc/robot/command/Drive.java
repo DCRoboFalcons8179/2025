d8b4d8ee4fe682ab7f30ccf62a31fa82cc49124c
MADMAN-Modding <mad@kmdtech.net>
2025-01-11 10:30:48 -0500

    changed names

--- src/main/java/frc/robot/command/Drive.java
+++ src/main/java/frc/robot/command/Drive.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.command;
 
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.DriveSub;
+import frc.robot.subsystem.DriveSub;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class Drive extends Command {
