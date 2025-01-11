aacf7c82f06a1ffdf835c0d364189e188f1dd34f
SloppyChar <char@gmail.com>
2025-01-11 11:47:33 -0500

    i made the motor class (wow so cool; im proud of you)

---
+++ src/main/java/frc/robot/commands/Elevator.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+public class Elevator {
+    private double elevatorSpeed;
+
+
+
+    //do not implement just yet
+    //private double elevatorPOS;
+    
+    public Elevator(int motorID, double elevatorSpeed){
+        
+    }
+
+}
+
+/* 
+what im gonna need
+
+elevator motor (neo)
+morot speed
+stop elevator when limit switch pressed
+controll motor goin gup/down
+maybe track current elecvatuoj\n
+*/
