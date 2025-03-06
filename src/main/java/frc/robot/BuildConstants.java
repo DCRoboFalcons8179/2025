a2be56b18fa85c30e628f638e0ec4bbb312eb8ab
robofalcons8179 <dcrobotics@gmail.com>
2025-03-05 20:25:29 -0500

    Merge

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,30 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 94;
-  public static final String GIT_SHA = "fd8a691a5df2f7306463f8aeb34a1695cf70c2cd";
-  public static final String GIT_DATE = "2025-03-03 20:31:53 EST";
+  public static final int GIT_REVISION = 99;
+  public static final String GIT_SHA = "3726042b59e7490285ac4666dc43236f4a83e9ce";
+  public static final String GIT_DATE = "2025-03-05 19:15:31 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-04 19:49:11 EST";
-  public static final long BUILD_UNIX_TIME = 1741135751615L;
+  public static final String BUILD_DATE = "2025-03-05 19:28:20 EST";
+  public static final long BUILD_UNIX_TIME = 1741220900440L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
+
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 98;
+  public static final String GIT_SHA = "d68411092fbdfde7be374d48111b76c2ce917f4c";
+  public static final String GIT_DATE = "2025-03-04 20:26:35 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-03-05 20:15:14 EST";
+  public static final long BUILD_UNIX_TIME = 1741223714340L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ src/main/java/frc/robot/commands/MoveHook.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.HookSub;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class MoveHook extends Command {
+
+  private DoubleSupplier hookPower;
+  private HookSub hookSub;
+
+  // This hopefully works for the control box but idk if it will
+  BooleanSupplier switch1Pressed;
+  BooleanSupplier switch2Pressed;
+
+  public void HookControl(BooleanSupplier switch1Pressed, BooleanSupplier switch2Pressed) {
+    this.switch1Pressed = switch1Pressed;
+    this.switch2Pressed = switch2Pressed;
+  }
+
+  /** Creates a new MoveHook. */
+  public MoveHook(DoubleSupplier hookPower, HookSub hookSub) {
+    this.hookPower = hookPower;
+    this.hookSub = hookSub;
+
+    addRequirements(hookSub);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // Moves the Hook
+    double downPower = switch1Pressed.getAsBoolean() && switch2Pressed.getAsBoolean() ? Constants.HookInfo.hookDown : Constants.HookInfo.hookStop;
+    hookSub.driveHook(downPower);
+    double upPower = !switch1Pressed.getAsBoolean() && switch2Pressed.getAsBoolean() ? Constants.HookInfo.hookUp : Constants.HookInfo.hookStop;
+    hookSub.driveHook(upPower);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
