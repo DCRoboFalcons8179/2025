212d91fe7e9bf7a3af58bd7f13ce6bdfb6cd2d07
MADMAN-Modding <mad@kmdtech.net>
2025-03-13 21:31:53 -0400

    Implement GetAuton class for autonomous command selection and update RobotContainer to utilize it

---
+++ src/main/java/frc/robot/GetAuton.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class GetAuton {
+    public static Command getAuton(int autonID) {
+        switch (autonID) {
+            case 0:
+                return null;
+            default:
+                return null;
+        }
+    }
+}
